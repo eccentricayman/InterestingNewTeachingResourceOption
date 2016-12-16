ed6734110376b0f4114cae926f96a9af614a12b7
Yikai <ywang8@stuy.edu>
2016-12-16 13:19:12 -0500

    more improvements

--- intro.js
+++ intro.js
@@ -1,4 +1,4 @@
-function buttonAction() {
+function buttonAction1() {
     var list = document.getElementById("thelist").children;
     var li = document.createElement("li");
     li.innerHTML = "item " + list.length;
@@ -21,21 +21,21 @@ function updateListlength() {
 }
 
 button1 = document.getElementById('b');
-button1.addEventListener("click", buttonAction);
+button1.addEventListener("click", buttonAction1);
 
 var ctr = 0;
-var list = document.getElementById("thelist").children;
-while (ctr < list.length) {
+var listItems = document.getElementById("thelist").children;
+while (ctr < listItems.length) {
     listItems[ctr].addEventListener("mouseover", changeTitle);
     listItems[ctr].addEventListener("mouseout", changeTitleBack);
     ctr++;
 }
 
 var fib = function(i) {
-  var a = 1; var b = 0; var temp;
+  var a = 1; var b = 0; var temp = 0;
 
-  for(; i >= 0; i--) {
-    temp = a + b;
+  for(; i > 0; i--) {
+    temp = a;
     a = a + b;
     b = temp;
   }
@@ -43,6 +43,13 @@ var fib = function(i) {
   return temp;
 }
 
-var list = document.getElementById("ol-ibonacci").children;
+function buttonAction2() {
+    var list = document.getElementById("ol-ibonacci").children;
+    var li = document.createElement("li");
+    li.innerHTML = fib(list.length+1);
+
+    document.getElementById("ol-ibonacci").appendChild(li);
+}
+
 button2 = document.getElementById('b-ibonacci');
-button2.addEventListener("click", fib(list.length));
+button2.addEventListener("click", buttonAction2);
