ba30cd2f768d7deb4e3879ffce859c0ea3f31538
eccentricayman <aahmed3@stuy.edu>
2016-12-16 12:37:15 -0500

    done with hover and add to list

--- intro.js
+++ intro.js
@@ -1,19 +1,27 @@
 function buttonAction() {
-    var list = document.getElementsByTagName("li");
-    var index = 0;
-    for (i = 0 ; i < list.length ; i++) {
-        index = i;
-    }
+    var list = document.getElementById("thelist").children;
     var li = document.createElement("li");
-    var item = document.createTextNode("item " + (index + 1))
+    var item = document.createTextNode("item " + list.length);
     li.appendChild(item);
     document.getElementById("thelist").appendChild(li);
+}
+
+function changeTitle() {
+    document.getElementById("h").innerHTML = this.innerHTML;
+}
+
+function changeTitleBack() {
+    document.getElementById("h").innerHTML = "Hello World!";
 }
 
 button = document.getElementById('b');
 button.addEventListener("click", buttonAction);
 
-var listItems = document.getElementsByTagName("li")
-for (listItem in ListItems) {
-    
+var listItems = document.getElementsByTagName("li");
+var ctr = 0;
+while (ctr < listItems.length) {
+    listItems[ctr].addEventListener("mouseover", changeTitle);
+    listItems[ctr].addEventListener("mouseout", changeTitleBack);
+    ctr++;
 }
+
