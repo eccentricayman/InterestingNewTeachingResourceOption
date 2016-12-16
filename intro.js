ab68c4c6d696877846c735105d40f1a51724359e
eccentricayman <aahmed3@stuy.edu>
2016-12-16 12:14:45 -0500

    intro html template

--- intro.js
+++ intro.js
@@ -1,1 +1,19 @@
-a
+function buttonAction() {
+    var list = document.getElementsByTagName("li");
+    var index = 0;
+    for (i = 0 ; i < list.length ; i++) {
+        index = i;
+    }
+    var li = document.createElement("li");
+    var item = document.createTextNode("item " + (index + 1))
+    li.appendChild(item);
+    document.getElementById("thelist").appendChild(li);
+}
+
+button = document.getElementById('b');
+button.addEventListener("click", buttonAction);
+
+var listItems = document.getElementsByTagName("li")
+for (listItem in ListItems) {
+    
+}
