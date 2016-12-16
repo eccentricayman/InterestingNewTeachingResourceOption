306e9d1083b97ba577f4435a0d9abf4700f22110
Yikai <ywang8@stuy.edu>
2016-12-16 12:48:53 -0500

    updated stuff

--- intro.js
+++ intro.js
@@ -1,8 +1,7 @@
 function buttonAction() {
     var list = document.getElementById("thelist").children;
     var li = document.createElement("li");
-    var item = document.createTextNode("item " + list.length);
-    li.appendChild(item);
+    li.innerHTML = "item " + list.length;
     document.getElementById("thelist").appendChild(li);
 }
 
@@ -26,7 +25,7 @@ while (ctr < listItems.length) {
 }
 
 var fib = function(i) {
-  var a = 0; var b = 0; var temp;
+  var a = 1; var b = 0; var temp;
 
   for(; i >= 0; i--) {
     temp = a + b;
